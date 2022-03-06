699103d0154d0b3d83d6d50a7b171e7ab3c2f5ee
Andres Melendez <codeslator@gmail.com>
2022-03-06 10:59:33 -0400

    Size Selector made with Vue + Vite + TypeScript

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import SizeSelector from './components/SizeSelector.vue';
+
+const selectedSize = ref<string>('none')
+
+</script>
+
+<template>
+  <h1>Selected Size: {{ selectedSize }}</h1>
+  <size-selector :selected-size="selectedSize" @on-size-change="selectedSize = $event" />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+button {
+  width: 75px;
+  padding: 5px;
+  border: none;
+  border-radius: 5px;
+  margin-right: 5px;
+  cursor: pointer;
+}
+
+.active {
+  background-color: #000;
+  color: #fff;
+  transform: all .3s ease-out;
+}
+</style>
---
+++ src/components/SizeSelector.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { toRefs } from 'vue';
+
+const sizes: string[] = [
+  'XS',
+  'S',
+  'M',
+  'L',
+  'XL',
+  'XXL',
+];
+
+interface Props {
+  selectedSize: string;
+};
+
+interface Emits {
+  (event: 'onSizeChange', value: string): void,
+}
+
+const props = defineProps<Props>();
+const emits = defineEmits<Emits>()
+
+toRefs(props);
+</script>
+
+<template>
+  <div>
+    <button
+      type="button"
+      v-for="(size, index) in sizes"
+      :key="index"
+      :class="{ 'active': selectedSize === size }"
+      @click="emits('onSizeChange', size)"
+    >
+      {{ size }}
+    </button>
+  </div>
+</template>
+
+<style>
+</style>
